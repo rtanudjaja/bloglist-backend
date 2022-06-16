712c96cc415213dae18d06db73eec5bc39a5a3de
Ricky Tanudjaja CT <ricky.tanudjaja@cartrack.com>
2022-06-16 15:39:41 +0700

    finish helper functions and unit tests, step3

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -61,3 +61,102 @@ describe('total likes', () => {
     expect(result).toBe(100)
   })
 })
+
+describe('favourite blog', () => {
+  test('from an empty list return empty object', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual({})
+  })
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f6',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://www.cs.utexas.edu/users/EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  test('from a list that has only one blog, return that particular blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    expect(result).toEqual({
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12
+    })
+  })
+
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'The Alchemist',
+      author: 'Paulo Coelho',
+      url: 'https://www.amazon.com/Alkymisten-Alchemist-Paulo-Coelho/dp/8776040011',
+      likes: 88,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17fA',
+      title: 'Wings of Fire',
+      author: 'Tui T. Sutherland',
+      url: 'https://www.amazon.com/Wings-Fire-Graphix-Box-Books/dp/1338796879/',
+      likes: 7,
+      __v: 0
+    }
+  ]
+
+  test('from a list of blogs, return the blog which has the most likes', () => {
+    const result = listHelper.favoriteBlog(listWithThreeBlogs)
+    expect(result).toEqual({
+      title: "The Alchemist",
+      author: "Paulo Coelho",
+      likes: 88
+    })
+  })
+
+  const listWithThreeSameLikesBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'The Alchemist',
+      author: 'Paulo Coelho',
+      url: 'https://www.amazon.com/Alkymisten-Alchemist-Paulo-Coelho/dp/8776040011',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17fA',
+      title: 'Wings of Fire',
+      author: 'Tui T. Sutherland',
+      url: 'https://www.amazon.com/Wings-Fire-Graphix-Box-Books/dp/1338796879/',
+      likes: 7,
+      __v: 0
+    }
+  ]
+
+  test('from a list with many top favorites, return one of the blog with most likes', () => {
+    const result = listHelper.favoriteBlog(listWithThreeSameLikesBlogs)
+    expect(result).toEqual({
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 7
+    })
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,7 +14,27 @@ const totalLikes = (blogs) => {
   }
 }
 
+const favoriteBlog = (blogs) => {
+  if(blogs?.length === 0) {
+    return {}
+  } else if(blogs?.length === 1) {
+    return {
+      title: blogs[0]?.title,
+      author: blogs[0]?.author,
+      likes: blogs[0]?.likes,
+    }
+  } else {
+    const sortedBlogs = blogs.slice().sort((a,b) => Number(b?.likes) - Number(a?.likes))
+    return {
+      title: sortedBlogs[0]?.title,
+      author: sortedBlogs[0]?.author,
+      likes: sortedBlogs[0]?.likes,
+    }
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
