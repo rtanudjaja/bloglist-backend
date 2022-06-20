1b1ca7b1a2b79d629e268f586d7f888def5d8edf
Ricky Tanudjaja CT <ricky.tanudjaja@cartrack.com>
2022-06-20 19:49:48 +0700

    finish Blog list tests, step 4

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const helper = require('../utils/list_helper')
@@ -53,6 +54,24 @@ test('making an HTTP POST request to the /api/blogs url successfully creates a n
   })
   expect(blogsArr).toHaveLength(helper.initialBlogs.length + 1)
   expect(blogsArr).toContainEqual(newBlog)
+})
+
+test('if the likes property is missing from the request, it will default to the value 0', async () => {
+  const newBlog = {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://www.cs.utexas.edu/users/EWD/transcriptions/EWD08xx/EWD808.html',
+  }
+  
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const likes = _.find(response.body, { ...newBlog })?.likes
+  expect(likes).toBe(0)
 })
 
 afterAll(() => {
