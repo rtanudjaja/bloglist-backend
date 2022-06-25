ec6f10bda491a95a3caebd8f53be5ed463010d92
Ricky Tanudjaja CT <ricky.tanudjaja@cartrack.com>
2022-06-25 17:11:47 +0700

    finish bloglist expansion, step7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -8,24 +17,27 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const usersAtStart = await User.find({})
-  // const user = await User.findById(body.userId)
-  const user = usersAtStart[1]
-
-  const blog = new Blog({
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes,
-    user: user.id
-  })
-  if(!blog.title && !blog.url) {
-    return response.sendStatus(400)
-  }
-  if(!blog.likes) {
-    blog.likes = 0
-  }
   try {
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
+      title: request.body.title,
+      author: request.body.author,
+      url: request.body.url,
+      likes: request.body.likes,
+      user: user.id
+    })
+    if(!blog.title && !blog.url) {
+      return response.sendStatus(400)
+    }
+    if(!blog.likes) {
+      blog.likes = 0
+    }
     const result = await blog.save()
     user.blogs = user.blogs.concat(result.id)
     await user.save()
